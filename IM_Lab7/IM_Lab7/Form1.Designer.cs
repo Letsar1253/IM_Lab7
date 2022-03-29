529511e2f231e1dd835729e46b01c326ab870f29
PankovMA <pankovm2001@gmail.com>
2022-03-29 22:31:32 +0700

    .

---
+++ IM_Lab7/IM_Lab7/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace IM_Lab7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.countPersons = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.countСriminalСase = new System.Windows.Forms.NumericUpDown();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.countPersons)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.countСriminalСase)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.AxisX.Minimum = 0D;
+            chartArea2.AxisY.Minimum = 0D;
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Location = new System.Drawing.Point(315, 12);
+            this.chart1.Name = "chart1";
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(792, 485);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(79, 448);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(139, 49);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Старт";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // countPersons
+            // 
+            this.countPersons.Location = new System.Drawing.Point(180, 12);
+            this.countPersons.Name = "countPersons";
+            this.countPersons.Size = new System.Drawing.Size(120, 22);
+            this.countPersons.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Штат сотрудников";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Кол-во дел";
+            // 
+            // countСriminalСase
+            // 
+            this.countСriminalСase.Location = new System.Drawing.Point(180, 52);
+            this.countСriminalСase.Name = "countСriminalСase";
+            this.countСriminalСase.Size = new System.Drawing.Size(120, 22);
+            this.countСriminalСase.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1119, 509);
+            this.Controls.Add(this.countСriminalСase);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.countPersons);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.countPersons)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.countСriminalСase)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.NumericUpDown countPersons;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown countСriminalСase;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ IM_Lab7/IM_Lab7/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IM_Lab7
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Кол-во сотрудников
+        /// </summary>
+        private int _countPersons;
+        /// <summary>
+        /// Кол-во дел
+        /// </summary>
+        private int _countCriminalCases;
+        private int _time = 0;
+        private Random _random = new Random();
+        /// <summary>
+        /// Загруженность участка
+        /// </summary>
+        private double _siteСongestion;
+        /// <summary>
+        /// Квалификация
+        /// </summary>
+        private double _qualification;
+        /// <summary>
+        /// Опасность дел
+        /// </summary>
+        private double _dangerLvl;
+        private int _inCome;
+        private int _outCome;
+        private int _countSiteСongestion = 0;
+        private double _expandSite = 0;
+        private int _timeInvestigationCases;
+        private int _timeProcessingDeclaration;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (!timer1.Enabled)
+            {
+                _countPersons = (int)countPersons.Value;
+                _countCriminalCases = (int)countСriminalСase.Value;
+                _time = 0;
+                timer1.Start();
+                buttonStart.Text = "Стоп";
+            }
+            else
+            {
+                timer1.Stop();
+                buttonStart.Text = "Старт";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.AddXY(_time, _countCriminalCases);
+            _time++;
+
+            var randomValue = _random.NextDouble();
+
+            _qualification = randomValue;
+            _dangerLvl = randomValue;
+
+           
+            _inCome = _random.Next(3, 24);
+            _outCome = _random.Next(3, 24);
+            _countCriminalCases += _inCome;
+            _siteСongestion = _countCriminalCases /( _countPersons * 5 * _qualification) - _expandSite;
+
+            if (_siteСongestion > 0.95)
+            {
+                _countSiteСongestion++;
+            }
+            if (_countSiteСongestion > 5)
+            {
+                _expandSite = _siteСongestion * (1 + _random.NextDouble());
+                _countSiteСongestion = 0;
+            }
+
+            _countCriminalCases -= _outCome;
+
+            //_dangerLvl = 0;
+            //for (int i = 0; i < _countCriminalCases; i++)
+            //{
+
+            //}
+
+
+            //     Time++;
+
+            //if (Time % ExpandPeriod == 0)
+            //{
+            //    ExpandCoeff = random.NextDouble();
+            //    ExpandCoeffSumm += ExpandCoeff;
+
+            //    StaffAmount += (int)(ExpandCoeff * StaffAmount);
+            //    MaxPatients += (int)(ExpandCoeff * MaxPatients);
+            //    MaxInCome += (int)(ExpandCoeff * MaxInCome);
+            //}
+
+            //InCome = Math.Min(MaxPatients - PatientAmount - ReAplications, random.Next(0, MaxInCome));
+
+            //PatientAmount += InCome + ReAplications;
+
+            //CureTime = random.Next(MinAnalysisTime, MaxAnalysisTime);
+            ////* ((double)PatientAmount / MaxPatients);
+
+            //HospitalCapacity = PatientAmount / (double)MaxPatients;
+
+            //AllCureTime = (HospitalCapacity + 0.9) * CureTime;
+
+            //int movablePatients = Math.Min((int)(PatientAmount * AllCureTime / 30), PatientAmount);
+            //OutCome = (int)(movablePatients * 0.8);
+
+            //PatientAmount -= OutCome;
+
+            //chart.Series[0].Points.AddXY(Time, PatientAmount);
+
+            //label_AllCureTime.Text = Math.Round(AllCureTime, 3).ToString();
+            //label_Repeats.Text = ReAplications.ToString();
+            //label_ExpandCoeffSumm.Text = Math.Round(ExpandCoeffSumm, 2).ToString();
+            //label_HospitalCapacity.Text = Math.Round(HospitalCapacity * 100, 3) + " %";
+            //label_Income.Text = InCome.ToString();
+            //label_Outcome.Text = OutCome.ToString();
+
+            //ReAplications = (int)(movablePatients * 0.2);
+        }
+    }
+}
